bb2b82c0d9ec8b292df1abc54166f66e7ab59586
amirabourogaa <amirabourogaa0@gmail.com>
2020-08-17 15:53:16 +0100

    not yet completed

---
+++ project.js
@@ -0,0 +1,10 @@
+	$('#myBtn').click( function(){
+        var $body = $('body');
+        
+    $('div').hide()
+    
+    	var username = $('<div></div>');
+    
+    $tweet.appendTo($body);
+        
+      }
