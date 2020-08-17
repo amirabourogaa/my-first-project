bd0086c017de19aaef1c670b80c9f7eea06279b8
amirabourogaa <amirabourogaa0@gmail.com>
2020-08-17 23:01:47 +0100

    not finished yet

--- project.js
+++ project.js
@@ -1,10 +1,8 @@
-	$('#myBtn').click( function(){
-        var $body = $('body');
-        
-    $('div').hide()
-    
-    	var username = $('<div></div>');
-    
-    $tweet.appendTo($body);
-        
-      }
+	// this function is to change the background color
+function myFunction() {
+  var color=$('#inputcolor').val()
+$('body').css("background-color", color);
+console.log( ($('#inputcolor').val()))
+//$('#inputcolor').defaultValue='';
+}
+
